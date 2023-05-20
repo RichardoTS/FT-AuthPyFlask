953fed4a5f9968452bec174ecff4c816662e8deb
RichardoTS <ri.tapiasaez@gmail.com>
2023-05-20 15:55:36 +0000

    Minor improvements

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
+import { useHistory } from "react-router-dom";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
@@ -7,37 +8,29 @@ export const Login = () => {
 	const { store, actions } = useContext(Context);
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	let history = useHistory();
+
+	console.log("This is your token", store.token);
 
 	const handleClick = () => {
-		const options ={
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify({
-				"email": email,
-				"password": password
-			})
-		}
-		fetch ('https://3001-richardots-ftauthpyflas-t7wjn3fwe9t.ws-us97.gitpod.io/api/token', options)
-			.then( resp => {
-				if(resp.status === 200) return resp.json();
-				else alert("There has been some error");
-			})
-			.then()
-			.catch(error => {
-				console.log("There was an error!", error)
-			})
-	}
+		actions.login(email, password);
+
+	};
+
+	if (store.token && store.token != "" && store.token != undefined) history.push("/");
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Login</h1>
-			<div>
-				<input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-				<input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-				<button onClick={handleClick}>Login</button>
-			</div>
+			{store.token && store.token != "" && store.token != undefined ? (
+				"You are logged in with this token" + token
+			) : (
+				<div>
+					<input type="text" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+					<input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+					<button onClick={handleClick}>Login</button>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -20,16 +21,57 @@ const getState = ({ getStore, getActions, setStore }) => {
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
+			syncTokenFromSessionStore: () => {
+				const token = sessionStorage.getItem("token");
+				console.log("Application just loaded.")
+				if (token && token != "" && token != undefined) setStore({ token: token})
+
+			},
+			logout: () => {
+				sessionStorage.removeItem("token");
+				console.log("Logged out.")
+				setStore({ token: null})
+
+			},
+			login: async (email, password) => {
+				const options = {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json"
+					},
+					body: JSON.stringify({
+						email: email,
+						password: password
+					})
+				};
+				try {
+					const resp = await fetch('https://3001-richardots-ftauthpyflas-t7wjn3fwe9t.ws-us97.gitpod.io/api/token', options)
+					if (resp.status !== 200) {
+						alert("There has been some error");
+						return false;
+					}
+
+					const data = await resp.json()
+					console.log("This came from the back", data);
+					sessionStorage.setItem("token", data.access_token);
+					setStore({ token: data.access_token})
+					return true;
+				}
+				catch (error) {
+					console.log("There has been an error at loggin in", error)
+				}
+
+			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
